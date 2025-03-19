42f0785907f45f9d6914fd803472b960e91b74c9
CURSO.py <anachurycastro2001@gmail.com>
2025-03-18 21:39:31 -0500

    voy solucionado errore

--- appComunidadEducativa/urls.py
+++ appComunidadEducativa/urls.py
@@ -6,11 +6,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name="home"),
     path('informacion', views.informacion, name="informacion"),
-    path('inicio', views.inicio, name="inicio"),
+    path('inicio Sesion', views.inicio, name="inicio Sesion"),
     path('planes', views.planes, name="planes"),
     path('programas', views.programas, name="programas"),
     path('registro', views.registro, name="registro"),
     path('profesores', views.profesores, name="profesores"),
+    path('registro', views.registro, name="pro"),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- appComunidadEducativa/views.py
+++ appComunidadEducativa/views.py
@@ -2,18 +2,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"home.html")
+
 def informacion(request):
     return render(request,"informacion.html")
-def inicio(request):
-    return render(request,"inicio.html")
+
+def inicio (request):
+    return render(request,"inicio Sesion.html")
+
 def planes(request):
     return render(request,"planes.html")
+
 def programas(request):
     return render(request,"programas.html")
+
 def registro(request):
     return render(request,"registro.html")
+
 def profesores(request):
     return render(request,"profesore.html")
 
+def regitro(request):
+    return render(request,"regitro.html")
+    
 # Create your views her
 # Create your views here.
